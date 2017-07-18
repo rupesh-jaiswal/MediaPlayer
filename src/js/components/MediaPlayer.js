83191bff9320cb86610e19c8a159e6e231015d31
Rupesh Jaiswal <rupesh.jaiswal@sts.local>
2017-07-18 18:16:34 +0530

    Added plus functionality

--- src/js/components/MediaPlayer.js
+++ src/js/components/MediaPlayer.js
@@ -7,17 +7,33 @@ import Article from 'grommet/components/Article';
 import Section from 'grommet/components/Section';
 import Headline from 'grommet/components/Headline';
 import Box from 'grommet/components/Box';
-
-
+import Menu from 'grommet/components/Menu';
+import Button from 'grommet/components/Button';
+import CaretNextIcon from 'grommet/components/icons/base/CaretNext';
+import CaretPreviousIcon from 'grommet/components/icons/base/CaretPrevious';
+import AddIcon from 'grommet/components/icons/base/Add';
+import FormField from 'grommet/components/FormField';
+import TextInput from 'grommet/components/TextInput';
 
 class MediaPlayer extends Component {
   constructor () {
     super();
+    this._onChange = this._onChange.bind(this);
     this._onResponsive = this._onResponsive.bind(this);
+    this._onAddUrl = this._onAddUrl.bind(this);
+    this.state={};
   }
 
+  _onAddUrl(event) {
+    //this.setState({addVideo:event.currentTarget.value});
+    //this.props.dispatch(addVideoURL(this.state.addVideo));
+  }
   _onResponsive (responsive) {
     //this.props.dispatch(navResponsive(responsive));
+  }
+
+  _onChange(event) {
+    this.setState({addVideo:event.currentTarget.value});
   }
 
   render() {
@@ -32,13 +48,27 @@ class MediaPlayer extends Component {
             </Headline>
           </Section>
           <Box align='center'>
-            <object>
-                <param name="movie" value="http://www.youtube.com/v/ZuNNhOEzJGA&hl=fr&fs=1&rel=0&color1=0x006699&color2=0x54abd6&border=1"></param>
-                <embed src="http://www.youtube.com/v/ZuNNhOEzJGA&hl=fr&fs=1&rel=0&color1=0x006699&color2=0x54abd6&border=1" type="application/x-shockwave-flash" allowfullscreen="true" width="425" height="349"></embed>
-            </object>
+            <iframe width="560" height="315" src={this.state.addVideo} ></iframe>
             <Box colorIndex='grey-4' size='large'>
               <div width='425' height='349'>
-              Controls
+                Controls
+                <Menu direction='row' size='large'>
+                  <Button icon={<CaretPreviousIcon />} primary={true} plain={true} />
+                  <Button icon={<CaretNextIcon />} plain={true}/>
+                  <Menu responsive={true}
+                    primary={false}
+                    inline={false}
+                    icon={<AddIcon />}
+                    closeOnClick={false}>
+                    <FormField >
+                      <TextInput id='addVideo'
+                        name='addVideo'
+                        placeHolder='Enter video url'
+                        onDOMChange={this._onChange} />
+                    </FormField>
+                    <Button icon={<AddIcon />} primary={true} label="Add to Playlist" onClick={this._onAddUrl}/>
+                  </Menu>
+                </Menu>
               </div>
             </Box>
           </Box>
